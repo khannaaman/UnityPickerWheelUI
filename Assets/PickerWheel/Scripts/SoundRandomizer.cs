6db9140e6ae9f1193ad2dcb5e8e11d2f2147b279
aman <khanna_aman@yahoo.com>
2023-06-17 19:46:49 -0500

    working audio files version

---
+++ Assets/PickerWheel/Scripts/SoundRandomizer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundRandomizer : MonoBehaviour
+{
+	public AudioClip[] audioClips;
+	private AudioSource audioSource;
+	// Use this for initialization
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.Play();
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+
+    }
+}
+
